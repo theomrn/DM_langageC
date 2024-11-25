680274db68f7c4c48c211ac525468b6a82011b7f
Theomrn <theomorneau@icloud.com>
2024-11-25 09:48:43 +0100

    feat: add char in struct cell, update print value

--- mazeGenerator.c
+++ mazeGenerator.c
@@ -11,9 +11,8 @@ x,y are the coordonates of the cell
 */
 typedef struct{
     int north;
-    int south;
-    int est;
     int west;
+    char *value;
 } Cell;
 
 
@@ -44,15 +43,17 @@ Maze* initializeMaze(Maze *maze){
             exit(1);
         }
         maze->mazeTab = newTab;
+        int index;
         for (int i = 0;i<mazeWidht;i++){
             for (int j = 0;j<mazeHeight;j++){
-                int index = i*mazeHeight+j;
+                index = i*mazeWidht+j;
                 maze->mazeTab[index] = (Cell *)malloc(sizeof(Cell));
                 if (!maze->mazeTab[index]) {
                     perror("erreur alocation mazeCell");
                     exit(1);
                 }
-                maze->mazeTab[index]->est = maze->mazeTab[index]->north = maze->mazeTab[index]->south = maze->mazeTab[index]->west = 1;
+                maze->mazeTab[index]->north = maze->mazeTab[index]->west = 1;
+                maze->mazeTab[index]->value = " ";
             }
         }
     }
@@ -60,40 +61,29 @@ Maze* initializeMaze(Maze *maze){
     return maze;
 }
 
-/* print the maze
-void printMaze(Maze *maze){
-    for (int i = 0;i<maze->width;i++){
-        for (int j = 0;j<maze->height;j++){
-            printf("%d ",maze->mazeTab[i*mazeHeight+j]->north);
-        }
-        printf("\n");
-    }
-}
-*/
-
 void printMaze(Maze *maze) {
     for (int i = 0; i < maze->height; i++) {
         // Top line north direction
         for (int j = 0; j < maze->width; j++) {
             int index = i * maze->width + j;
-            printf(" ");
+            printf("%s",maze->mazeTab[i*mazeWidht+j]->value);
             printf(maze->mazeTab[index]->north == 1 ? "_" : " ");
         }
         printf("\n");
 
-        // west line , space , est line
+        // west line , value
         for (int j = 0; j < maze->width; j++) {
             int index = i * maze->width + j;
             printf(maze->mazeTab[index]->west == 1 ? "|" : " "); // west wall
-            printf(" ");  // space
+            printf("%s",maze->mazeTab[i*mazeWidht+j]->value);  // space
         }
-        printf("|\n");  // Right wall end of the line
+        printf("|\n");  // Right wall, end of the line
 
         // south line
         if (i == maze->height - 1) {
             for (int j = 0; j < maze->width; j++) {
                 int index = i * maze->width + j;
-                printf(maze->mazeTab[index]->south == 1 ? " _" : "  ");
+                printf(" _");
             }
             printf("\n");
         }
